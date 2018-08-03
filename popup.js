2006a203fe4011c6e416894d0b52650c31df729c
Nathan Phan <nathphan510@gmail.com>
2018-08-03 13:54:34 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
-  let text = document.getElementById('name');
+const url = 'https://api.fortnitetracker.com/v1/profile/pc/daddy%20natty';
+const params = {
+  headers:{
+    "trn-api-key": "e4060f2a-df44-45a7-8ca5-3938e2c2a631"
+  },
+  method: "GET"
+};
 
-  chrome.storage.sync.get('epicName', function(data) {
-    text.textContent = data.name;
-  });
+fetch(url,params)
+  .then(data=>{return data.json()})
+  .then(res=>{console.log(res)});
