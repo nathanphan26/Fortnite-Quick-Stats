f711fa2295ba93419b56689520bd0cd4ee2f395e
Nathan Phan <nathphan510@gmail.com>
2018-08-05 21:25:19 -0700

    v1 testing

--- popup.js
+++ popup.js
@@ -1,15 +1,30 @@
-const params = {
-  headers:{
-    "trn-api-key": "e4060f2a-df44-45a7-8ca5-3938e2c2a631"
-  },
-  method: "GET"
-};
+// const params = {
+//   headers:{
+//     "trn-api-key": "e4060f2a-df44-45a7-8ca5-3938e2c2a631"
+//   },
+//   method: "GET"
+// };
 
+var params;
+
+// var monkey;
 
 $(document).ready(function() {
 	$("#name").hide();
 	$("#allStats").hide();
+
+	$.get("includes/help.stats", function(data) {
+		// monkey = data;
+		params = {
+			headers:{
+				"trn-api-key": data
+			},
+			method: "GET"
+		}
+	});
 });
+
+
 
 function getTier(rating, mode){
 	var img;
@@ -36,7 +51,7 @@ function getTier(rating, mode){
 	else if(rating >= 4000 && rating < 4500){
 		img.src = 'images/tier4.png';
 	}
-	else if(rating >= 4500 && rating < 5000){
+	else if(rating >= 4500){
 		img.src = 'images/tier5.png';
 	}
 	// else if(rating >= 1500 && rating < 3000){
@@ -78,17 +93,8 @@ window.addEventListener('load', function load(event){
 				  	document.getElementById('killsSolo').innerHTML = res.stats.curr_p2.kills.displayValue;
 				  	document.getElementById('winPerSolo').innerHTML = res.stats.curr_p2.winRatio.displayValue+"%";
 				  	document.getElementById('kdSolo').innerHTML = res.stats.curr_p2.kd.displayValue;
-				  	// document.getElementById('top10Solo').innerHTML = res.stats.curr_p2.top10.displayValue;
-				  	// document.getElementById('kpmSolo').innerHTML = res.stats.curr_p2.kpg.displayValue;
-				  	// document.getElementById('top25Solo').innerHTML = res.stats.curr_p2.top25.displayValue;
-				  	// document.getElementById('scoreSolo').innerHTML = res.stats.curr_p2.score.displayValue;
 				  	document.getElementById('soloRank').innerHTML = res.stats.curr_p2.trnRating.displayValue;
-				  	// if(res.stats.curr_p2.trnRating.valueInt >= 1500 && res.stats.curr_p2.trnRating.valueInt <3000){
-				  	// 	var img = document.createElement("img");
-				  	// 	img.src = 'images/tier2.png';
-				  	// 	img.classList.add('tier');
-				  	// 	document.getElementById('ratingSolo').insertBefore(img, document.getElementById('ratingSolo').firstChild);
-				  	// }
+
 				  	getTier(res.stats.curr_p2.trnRating.valueInt, 1);
 				  	
 				  	// Duo stats
@@ -96,11 +102,9 @@ window.addEventListener('load', function load(event){
 				  	document.getElementById('killsDuo').innerHTML = res.stats.curr_p10.kills.displayValue;
 				  	document.getElementById('winPerDuo').innerHTML = res.stats.curr_p10.winRatio.displayValue+"%";
 				  	document.getElementById('kdDuo').innerHTML = res.stats.curr_p10.kd.displayValue;
-				  	// document.getElementById('top5Duo').innerHTML = res.stats.curr_p10.top5.displayValue;
-				  	// document.getElementById('kpmDuo').innerHTML = res.stats.curr_p10.kpg.displayValue;
-				  	// document.getElementById('top12Duo').innerHTML = res.stats.curr_p10.top12.displayValue;
-				  	// document.getElementById('scoreDuo').innerHTML = res.stats.curr_p10.score.displayValue;
+
 				  	document.getElementById('duoRank').innerHTML = res.stats.curr_p10.trnRating.displayValue;
+
 				  	getTier(res.stats.curr_p10.trnRating.valueInt, 2);
 
 				  	//Squad stats
@@ -108,11 +112,8 @@ window.addEventListener('load', function load(event){
 				  	document.getElementById('killsSquad').innerHTML = res.stats.curr_p9.kills.displayValue;
 				  	document.getElementById('winPerSquad').innerHTML = res.stats.curr_p9.winRatio.displayValue+"%";
 				  	document.getElementById('kdSquad').innerHTML = res.stats.curr_p9.kd.displayValue;
-				  	// document.getElementById('top3Squad').innerHTML = res.stats.curr_p9.top3.displayValue;
-				  	// document.getElementById('kpmSquad').innerHTML = res.stats.curr_p9.kpg.displayValue;
-				  	// document.getElementById('top6Squad').innerHTML = res.stats.curr_p9.top6.displayValue;
-				  	// document.getElementById('scoreSquad').innerHTML = res.stats.curr_p9.score.displayValue;
 				  	document.getElementById('squadRank').innerHTML = res.stats.curr_p9.trnRating.displayValue;
+
 				  	getTier(res.stats.curr_p9.trnRating.valueInt, 3);
 
 				  	document.getElementById('inputName').value = '';
